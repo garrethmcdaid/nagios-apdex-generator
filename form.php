4bf46ae6f062973575e5b25c7ba179d2e79ba559
Garreth McDaid <garreth.mcdaid@nightbluefruit.com>
2014-05-23 12:29:26 +0100

    Interactive update

--- form.php
+++ form.php
@@ -1,12 +1,12 @@
 <?php
 
-$q = "SELECT * FROM logentries WHERE date > date_sub(now(), interval 365 day) AND service NOT LIKE '%NOTIFICATION%'";
+$q = "SELECT * FROM logentries WHERE date > date_sub(now(), interval 3 day) AND service NOT LIKE '%NOTIFICATION%'";
 $log = $DB->get_results($q);
 $services = array();
 
 foreach ($log as $k => $v) {
 	$service = explode(": ",$v->service);
-        if (substr($service[1],"SERVICE")) {
+        if (strstr($service[0],"SERVICE")) {
 		$services[$service[1]] = 1;
         }		
 }
@@ -16,24 +16,21 @@ foreach ($log as $k => $v) {
 <form method="POST">
 
 <select name="service">
-	<option value="latam.mttnow.com">latam.mttnow.com
+	<?php
+		foreach ($services as $k => $v) {
+			echo "<option value='" . $k . "'>" . $k;
+		}
+	?>
 </select>
 
-<div style="height:20px;"></div>
-
-<select name="monitor">
-	<option value="Apdex Monitor">Apdex Monitor
-	<option value="Apdex - Email">Apdex - Email
-	<option value="Apdex - Flight Status">Apdex - Flight Status
-</select>
+<input type="hidden" name="step" value="service">
 
 <div style="height:20px;"></div>
 
 <input type="hidden" name="start" value="<?php echo date('Y-m-d', strtotime('yesterday')); ?>">
 <input type="hidden" name="end" value="<?php echo date('Y-m-d', strtotime('yesterday')); ?>">
 
-
-<input type="submit" name="submit" value="Get Score">
+<input type="submit" name="submit" value="Get Scores">
 
 
 </form>
--- index.php
+++ index.php
@@ -5,6 +5,8 @@ date_default_timezone_set('Europe/Dublin');
 include('db.class.php');
 global $DB;
 
+$_REQUEST['service'] = 'latam.mttnow.com';
+
 //DATABASE
 $GLOBALS['dbserver'] = '127.0.0.1';
 $GLOBALS['dbname'] = 'nagios';
@@ -13,150 +15,173 @@ $GLOBALS['dbpass'] = '';
 
 include('header.php');
 
+$q = "SELECT * FROM logentries WHERE date > date_sub(now(), interval 366 day) AND service NOT LIKE '%NOTIFICATION%'";
+$log = $DB->get_results($q);
+$monitors = array();
+
+foreach ($log as $k => $v) {
+	$monitors[$v->monitor] = 1;
+}
+
+
 if (empty($_REQUEST['service'])) {
 	include('form.php');	
 } else {
 
-	//VARS
-	$total_ok = 0;
-	$total_warning = 0;
-	$total_critical = 0;
 	
-	if (!empty($_REQUEST)) {
-		$start = $_REQUEST['start'];
-		$end = $_REQUEST['end'];
-		$service = $_REQUEST['service'];
-		$monitor = $_REQUEST['monitor'];
-	} else {
-		$start = $argv[1];
-		$end = $argv[2];
-		$service = $argv[3];
-		$monitor = $argv[4];
-	}
-	
-	$samples = number_format((date('U',strtotime($end . " 23:59:59")) - date('U',strtotime($start . " 00:00:00"))) / 300,"0",".","");
-		
-	$q = "SELECT * FROM logentries WHERE date >= '" . $start . " 00:00:00' AND date <= '" . $end . " 23:59:59' AND service LIKE '%" . $service . "%' AND monitor = '" . $monitor . "'";
-	$data = $DB->get_results($q);
-	
-	echo $q . "<br>";
-	
-	//print_r($data);
-	
-	$q = "SELECT * FROM logentries WHERE date < '" . $data[0]->date . "' AND service LIKE '%" . $service . "%' AND monitor = '" . $monitor . "' ORDER BY date DESC LIMIT 1";
-	$r = $DB->get_result($q);
-	
-	echo $q . "<br>";
-	
-	if (!$r) die("Insufficient data<br>");
-	
-	$ds = date('U',strtotime($start . " 00:00:00"));
-	
-	$i = $data[0]->seconds - $ds;
-	
-	switch($r->type) {
-		
-		case "OK":
-			$total_ok = $i;
-			break;
-		case "WARNING":
-			$total_warning = $i;
-			break;
-		case "CRITICAL":
-			$total_critical = $i;
-			break;
-		
-	}
-	
-	foreach ($data as $k => $v) {
-	
-		if ($k === 0) continue;
-	
-		$p = $k -1;
-		//echo "Previous key = " . $p . "<br>"; 
-		
-		//echo "Current key seconds = " . $v->seconds . "<br>";
-		//echo "Previous key seconds = " . $data[$p]->seconds . "<br>";
-	
-		
-		$i = $v->seconds - $data[$p]->seconds;
-		//echo "Interval = " . $i . "<br>";
-	
-		switch($data[$p]->type) {
-			
-			case "OK":
-				//echo "Previous type = OK<br>";
-				$total_ok = $total_ok + $i;
-				break;
-			case "WARNING":
-				//echo "Previous type = WARNING<br>";
-				$total_warning = $total_warning + $i;
-				break;
-			case "CRITICAL":
-				//echo "Previous type = CRITICAL<br>";
-				$total_critical = $total_critical + $i;
-				break;
-			
-		}
-		
-	}
-	
-	$de = date('U',strtotime($end . " 23:59:59"));
-	
-	$i = $de - end($data)->seconds;
-	
-	switch(end($data)->type) {
-		
-		case "OK":
-			$total_ok = $total_ok + $i;
-			break;
-		case "WARNING":
-			$total_warning = $total_warning + $i;
-			break;
-		case "CRITICAL":
-			$total_critical = $total_critical + $i;
-			break;
-		
-	}
-	
-	
-	
-	$samples_ok = number_format($total_ok/60,"0",".","")/5;
-	$samples_warning = number_format($total_warning/60,"0",".","")/5;
-	$samples_critical = number_format($total_critical/60,"0",".","")/5;
-	
-	echo "~~~~~~~~~~~~~~~~~~~<br>";
-	
-	echo "SAMPLES: " . $samples . "<br>";
-	
-	echo "~~~~~~~~~~~~~~~~~~~<br>";
-	
-	echo "SATISFIED SECS: " . $total_ok . "<br>";
-	echo "TOLERATING SECS: " . $total_warning . "<br>";
-	echo "FRUSTRATED SECS: " . $total_critical . "<br>";
-	
-	echo "~~~~~~~~~~~~~~~~~~~<br>";
-	
-	echo "SATISFIED MINS: " . number_format($total_ok/60,"0",".",",") . "<br>";
-	echo "TOLERATING MINS: " . number_format($total_warning/60,"0",".",",") . "<br>";
-	echo "FRUSTRATED MINS: " . number_format($total_critical/60,"0",".",",") . "<br>";
-	
-	echo "~~~~~~~~~~~~~~~~~~~<br>";
-	
-	echo "SATISFIED SAMPLES: " . $samples_ok . "<br>";
-	echo "TOLERATING SAMPLES: " . $samples_warning . "<br>";
-	echo "FRUSTRATED SAMPLES: " . $samples_critical . "<br>";
-	
-	echo "~~~~~~~~~~~~~~~~~~~<br>";
-	
-	$apdex = ($samples_ok + ($samples_warning/2))/$samples;
-	
-	echo "APDEX FORMULA: " . $samples_ok . " + " . "(" . $samples_warning . "/2) / " . $samples . "<br>";
-	
-	echo "APDEX SCORE: " . number_format($apdex,"4",".","") . "<br>";
+	$reports = array();
+	$reports['yesterday'] = array('yesterday','today');
+	$reports['last_7_days'] = array('7 days ago 00:00','today');
+	$reports['last_28_days'] = array('29 days ago 00:00','today');
+	$reports['last_92_days'] = array('93 days ago 00:00','today');
+	$reports['last_365_days'] = array('366 days ago 00:00','today');
 
-}
+	foreach ($monitors as $monitor => $v) {
 	
+		foreach ($reports as $report => $v) {
+		
+			//VARS
+			$total_ok = 0;
+			$total_warning = 0;
+			$total_critical = 0;
+			
+			$interval = date('U', strtotime($v[1])) - date('U', strtotime($v[0]));
+	
+			$samples = number_format($interval/300,"0",".","");
+			
+			$q = "SELECT * FROM logentries WHERE seconds >= " . date('U', strtotime($v[0])) . " AND seconds < " . date('U', strtotime($v[1])) . " AND service LIKE '%" . $_REQUEST['service'] . "%' AND monitor = '" . $monitor . "'";
+			$data = $DB->get_results($q);
+			
+			if (!$data) { echo("Insufficient data for " . $report . "<br>"); continue;} 
+		
+			echo $q . "<br>";
+		
+			//print_r($data);
+			
+			$q = "SELECT * FROM logentries WHERE date < '" . date('Y-m-d H:i:s',strtotime($v[0])) . "' AND service LIKE '%" . $_REQUEST['service'] . "%' AND monitor = '" . $monitor . "' ORDER BY date DESC LIMIT 1";
+			$r = $DB->get_result($q);
+		
+			echo $q . "<br>";
+		
+			if (!$r) { echo("Insufficient data for " . $report . "<br>"); continue;} 
+		
+			$ds = date('U',strtotime($v[0]));
+		
+			$i = $data[0]->seconds - $ds;
+			
+			echo $i . "<br>";
+		
+			switch($r->type) {
+			
+				case "OK":
+					$total_ok = $i;
+					break;
+				case "WARNING":
+					$total_warning = $i;
+					break;
+				case "CRITICAL":
+					$total_critical = $i;
+					break;
+			
+			}
+		
+			foreach ($data as $k => $v) {
+			
+				if ($k === 0) continue;
+			
+				$p = $k -1;
+				//echo "Previous key = " . $p . "<br>"; 
+				
+				//echo "Current key seconds = " . $v->seconds . "<br>";
+				//echo "Previous key seconds = " . $data[$p]->seconds . "<br>";
+			
+				
+				$i = $v->seconds - $data[$p]->seconds;
+				//echo "Interval = " . $i . "<br>";
+			
+				switch($data[$p]->type) {
+					
+					case "OK":
+						//echo "Previous type = OK<br>";
+						$total_ok = $total_ok + $i;
+						break;
+					case "WARNING":
+						//echo "Previous type = WARNING<br>";
+						$total_warning = $total_warning + $i;
+						break;
+					case "CRITICAL":
+						//echo "Previous type = CRITICAL<br>";
+						$total_critical = $total_critical + $i;
+						break;
+					
+				}
+				
+			}
+			
+			$de = date('U',strtotime($end . " 23:59:59"));
+			
+			$i = $de - end($data)->seconds;
+			
+			switch(end($data)->type) {
+				
+				case "OK":
+					$total_ok = $total_ok + $i;
+					break;
+				case "WARNING":
+					$total_warning = $total_warning + $i;
+					break;
+				case "CRITICAL":
+					$total_critical = $total_critical + $i;
+					break;
+				
+			}
+			
+			
+			
+			$samples_ok = number_format($total_ok/60,"0",".","")/5;
+			$samples_warning = number_format($total_warning/60,"0",".","")/5;
+			$samples_critical = number_format($total_critical/60,"0",".","")/5;
+			
+			
+			echo "~~~~~~~~~~~~~~~~~~~<br>";
+	
+			echo $monitor . " - " . $report . "<br>";
+			
+			echo "~~~~~~~~~~~~~~~~~~~<br>";
+			
+			echo "SAMPLES: " . $samples . "<br>";
+			
+			echo "~~~~~~~~~~~~~~~~~~~<br>";
+			
+			echo "SATISFIED SECS: " . $total_ok . "<br>";
+			echo "TOLERATING SECS: " . $total_warning . "<br>";
+			echo "FRUSTRATED SECS: " . $total_critical . "<br>";
+			
+			echo "~~~~~~~~~~~~~~~~~~~<br>";
+			
+			echo "SATISFIED MINS: " . number_format($total_ok/60,"0",".",",") . "<br>";
+			echo "TOLERATING MINS: " . number_format($total_warning/60,"0",".",",") . "<br>";
+			echo "FRUSTRATED MINS: " . number_format($total_critical/60,"0",".",",") . "<br>";
+			
+			echo "~~~~~~~~~~~~~~~~~~~<br>";
+			
+			echo "SATISFIED SAMPLES: " . $samples_ok . "<br>";
+			echo "TOLERATING SAMPLES: " . $samples_warning . "<br>";
+			echo "FRUSTRATED SAMPLES: " . $samples_critical . "<br>";
+			
+			echo "~~~~~~~~~~~~~~~~~~~<br>";
+			
+			$apdex = ($samples_ok + ($samples_warning/2))/$samples;
+			
+			echo "APDEX FORMULA: " . $samples_ok . " + " . "(" . $samples_warning . "/2) / " . $samples . "<br>";
+			
+			echo "APDEX SCORE: " . number_format($apdex,"4",".","") . "<br>";
+		
+	
+		}
+	}
+}
+		
 include('footer.php');
 
 
